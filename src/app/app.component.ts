f554449a547d3ea69e9b969065ca905a65d50db1
Boris Karabanov <karabanovbswork@gmail.com>
2018-09-29 21:02:19 +0500

    visual

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {Email} from './email';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +9,17 @@ import {Component} from '@angular/core';
 export class AppComponent {
   title = 'rtb-test-emails-editor';
 
-  addEmail() {
+  sharedEmails: Array<Email> = [];
 
+  private getRandomString(): string {
+    return Math.random().toString(36).substring(2, Math.random() * (15 - 3) + 3);
+  }
+
+  addEmail() {
+    this.sharedEmails.push(new Email(`${this.getRandomString()}@${this.getRandomString()}.${this.getRandomString()}`));
   }
 
   getEmailCount() {
-
+    alert(this.sharedEmails.length);
   }
 }
